34aac78958088a969dea5da14988b95c1654b488
wuhuilin5 <huilin.wu@hotmail.com>
2014-03-27 17:00:14 +0800

    add loadingManager

---
+++ Assets/Scripts/ILoadManger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+using UnityDemo.manager;
+
+namespace UnityDemo.interfaces.manager
+{
+    public interface ILoadManger
+    {
+        IEnumerator loadUrl(string url, int version, LoadFunishHandler callback = null, string filename = "" );  
+    }
+
+}
---
+++ Assets/Scripts/LoadManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using UnityDemo.interfaces.manager;
+using UnityEngine;
+
+namespace UnityDemo.manager
+{
+    public delegate void LoadFunishHandler(WWW www, string filename);
+
+    public class LoadManager : ILoadManger
+    {
+        private static ILoadManger instance;
+
+        private LoadManager() { }
+
+        public static ILoadManger getIntance()
+        {
+            if (instance == null)
+                instance = new LoadManager();
+
+            return instance;
+        }
+
+        public IEnumerator loadUrl(string url, int version, LoadFunishHandler callback = null, string filename = "")
+        {
+            WWW loader = WWW.LoadFromCacheOrDownload(url, version);
+
+            yield return loader;
+
+            if (loader.error != null){
+                Debug.Log(String.Format("Load Error:{0}", loader.error));
+            }
+            else {
+                if (callback != null)
+                    callback( loader, filename );
+            }
+        }
+    }
+}
